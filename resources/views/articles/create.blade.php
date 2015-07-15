52cd333c0a3fd78fee7491960b078a9a6ca67e3b
Kieu Thi Nguyen <kieung@hotmail.com>
2015-07-15 16:58:25 +0200

    Form added

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -8,8 +8,25 @@
 
 	{!! Form::open() !!}
 
-		{!! Form::label('name', 'Name:') !!}
-		{!! Form::text('name') !!}
+		<div class="form-group">
+
+			{!! Form::label('title', 'Title:') !!}
+			{!! Form::text('title', null, ['class' => 'form-control']) !!}
+
+		</div>
+
+		<div class="form-group">
+
+			{!! Form::label('body', 'Body:') !!}
+			{!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+
+		</div>
+
+		<div class="form-group">
+
+			{!! Form::submit('Add article', ['class' => 'btn - btn-primary form-control']) !!}
+		</div>
+
 
 	{!! Form::close() !!}
 
