239ec4826eec75816190972c6870e66737177a8f
Kieu Thi Nguyen <kieung@hotmail.com>
2015-07-15 13:49:41 +0200

    New controllers, new views, and added model

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+    //all attributes in here should be fields that can be changed or filled
+    //in by a user.
+
+    	'title',
+    	'body'
+
+    ];
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,5 @@
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
+
+Route::get('articles', 'ArticlesController@index');
